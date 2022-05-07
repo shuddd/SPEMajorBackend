b30da4f7a66c55dd194807cdfe38dc0b1f7fb21f
vishalgovil <vishalgovil5@gmail.com>
2022-05-08 02:16:55 +0530

    admin cors error

--- src/main/java/com/tripbuddy/start/controller/AdminController.java
+++ src/main/java/com/tripbuddy/start/controller/AdminController.java
@@ -2,15 +2,18 @@ package com.tripbuddy.start.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.tripbuddy.start.Entity.Admin;
 import com.tripbuddy.start.services.AdminService;
 
 
 
-
+@CrossOrigin
+@RestController
 public class AdminController {
 
 	@Autowired
